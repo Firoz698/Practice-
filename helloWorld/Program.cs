f334dccba9228cf64f9a93961d4c308e5748d066
Firoz698 <mdfirozahmed698@gmail.com>
2023-02-17 23:56:07 +0600

    Usring for Loop

--- helloWorld/Program.cs
+++ helloWorld/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int[] data = { 2, 3, 4, 5 };
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
